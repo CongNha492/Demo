ad6c258406d31730ebefdc10956af0cd873ac103
CongNha492 <dh51805206@student.stu.edu.vn>
2021-11-12 17:25:57 +0700

    them hieu vao source.cpp

--- source (1).cpp
+++ source (1).cpp
@@ -3,6 +3,9 @@ using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
 
+int hieu(int a, int b) {
+	return a - b;
+}
 int main()
 {
 	int a, b;
@@ -13,6 +16,20 @@ int main()
 	cin >> b;
 	cout << "Chon phep toan (+,-) ";
 	cin >> phepToan;
+
+	switch (phepToan)
+	{
+		case '-':
+		{
+			cout << "Hieu la: " << hieu(a, b);
+		}
+		default:
+		{
+			cout << "Phep toan khong hop le";
+			break;
+		}
+	}
+
 	system("pause");
 	return 0;
 }
